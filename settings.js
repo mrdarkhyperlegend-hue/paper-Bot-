5c8fe3be16ea1a16e6a23a83124ff60bcc7d722e
D.S.S.KARUNARATHNA <mrdarkhyperlegend@gmail.com>
2026-04-06 18:16:25 +0530

    Edu-Bot✨

---
+++ settings.js
@@ -0,0 +1,20 @@
+const settings = {
+    botName: 'EDU-BOT-LK',
+    ownerName: 'Suneth',
+    ownerNumber: '947xxxxxxxxx',
+    prefix: '.', 
+    
+    
+    menu: `🎓 *STUDY ASSISTANT BOT* 🎓
+
+පහත විධානයන් භාවිතා කරන්න:
+
+📂 *.search [විෂය]* - පේපර්ස් සර්ච් කිරීමට
+📄 *.notes* - නිබන්ධන ලබා ගැනීමට
+👨‍💻 *.owner* - බොට්ගේ හිමිකරු සම්බන්ධ කර ගැනීමට
+
+💡 පේපර් එකක් ලබා ගැනීමට අදාළ අංකය පමණක් එවන්න.
+`
+};
+
+module.exports = settings;
